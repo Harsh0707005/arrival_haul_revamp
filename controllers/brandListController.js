26e78656c9db7c6414ed5e59fe6dca3ec8f72fe2
Harsh0707005 <harshmaster07705@gmail.com>
2025-05-06 23:40:54 +0530

    brand,category discount

--- controllers/brandListController.js
+++ controllers/brandListController.js
@@ -1,5 +1,6 @@
 require('dotenv').config();
 const { PrismaClient } = require("@prisma/client");
+const { calculatePriceDifference } = require("../services/currencyConversion");
 const prisma = new PrismaClient();
 
 exports.getCommonBrands = async (req, res) => {
@@ -23,15 +24,65 @@ exports.getCommonBrands = async (req, res) => {
             }
         });
 
-        const formattedBrands = fullBrands.map(brand => ({
-            brand_id: brand.id,
-            brand_name: brand.name,
-            image: brand.image,
-            discount: 0,
-            is_negative: false,
-            createdAt: brand.createdAt,
-            updatedAt: brand.updatedAt
-        }))
+        const formattedBrands = [];
+
+        for (const brand of fullBrands) {
+            const sourceProducts = await prisma.product.findMany({
+                where: {
+                    country_id: source_country_id,
+                    brand_id: brand.id
+                },
+                select: {
+                    sku_id: true,
+                    price: true
+                }
+            });
+
+            const destinationProducts = await prisma.product.findMany({
+                where: {
+                    country_id: destination_country_id,
+                    brand_id: brand.id
+                },
+                select: {
+                    sku_id: true,
+                    price: true
+                }
+            });
+
+            const destinationProductMap = new Map();
+            destinationProducts.forEach(p => destinationProductMap.set(p.sku_id, p.price));
+
+            let sourceTotal = 0;
+            let destinationTotal = 0;
+
+            for (const sp of sourceProducts) {
+                if (destinationProductMap.has(sp.sku_id)) {
+                    sourceTotal += sp.price;
+                    destinationTotal += destinationProductMap.get(sp.sku_id);
+                }
+            }
+
+            let discount = 0;
+            if (sourceTotal > 0 && destinationTotal > 0) {
+                const diff = await calculatePriceDifference(
+                    source_country_id,
+                    destination_country_id,
+                    sourceTotal,
+                    destinationTotal
+                );
+                discount = diff.percentageDifference;
+            }
+
+            formattedBrands.push({
+                brand_id: brand.id,
+                brand_name: brand.name,
+                image: brand.image,
+                discount: discount,
+                is_negative: false,
+                createdAt: brand.createdAt,
+                updatedAt: brand.updatedAt
+            });
+        }
 
         return res.json({
             message: "Brands found in both countries",
--- controllers/categoryListController.js
+++ controllers/categoryListController.js
@@ -1,5 +1,6 @@
 require('dotenv').config();
 const { PrismaClient } = require("@prisma/client");
+const { calculatePriceDifference } = require("../services/currencyConversion");
 const prisma = new PrismaClient();
 
 exports.getCommonCategories = async (req, res) => {
@@ -23,23 +24,76 @@ exports.getCommonCategories = async (req, res) => {
             }
         });
 
+        const formattedCategories = [];
 
-        const formattedCategories = fullCategories.map(category => ({
-            category_id: category.id,
-            category_name: category.name,
-            discount: 0,
-            is_negative: false,
-            createdAt: category.createdAt,
-            updatedAt: category.updatedAt
-        }))
+        for (const category of fullCategories) {
+            const sourceProducts = await prisma.product.findMany({
+                where: {
+                    country_id: source_country_id,
+                    category_id: category.id
+                },
+                select: {
+                    sku_id: true,
+                    price: true
+                }
+            });
+
+            const destinationProducts = await prisma.product.findMany({
+                where: {
+                    country_id: destination_country_id,
+                    category_id: category.id
+                },
+                select: {
+                    sku_id: true,
+                    price: true
+                }
+            });
+
+            const destinationProductMap = new Map();
+            destinationProducts.forEach(p => destinationProductMap.set(p.sku_id, p.price));
+
+            let sourceTotal = 0;
+            let destinationTotal = 0;
+
+            for (const sp of sourceProducts) {
+                if (destinationProductMap.has(sp.sku_id)) {
+                    sourceTotal += sp.price;
+                    destinationTotal += destinationProductMap.get(sp.sku_id);
+                }
+            }
+
+            let discount = 0;
+            if (sourceTotal > 0 && destinationTotal > 0) {
+                const diff = await calculatePriceDifference(
+                    source_country_id,
+                    destination_country_id,
+                    sourceTotal,
+                    destinationTotal
+                );
+                discount = diff.percentageDifference;
+            }
+
+            formattedCategories.push({
+                category_id: category.id,
+                category_name: category.name,
+                discount: discount,
+                is_negative: false,
+                createdAt: category.createdAt,
+                updatedAt: category.updatedAt
+            });
+        }
 
         return res.json({
             message: "Categories found in both countries",
+            success: true,
             categories: formattedCategories
         });
 
     } catch (err) {
         console.error("Error in getCommonCategories:", err);
-        res.status(500).json({ error: "Internal Server Error" });
+        res.status(500).json({
+            success: false,
+            message: "Internal Server Error"
+        });
     }
 };
